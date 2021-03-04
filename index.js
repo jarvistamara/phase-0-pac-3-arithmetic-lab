1c8e0e340bfb74cdb7927396167dd5be28889ec3
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-04 14:19:54 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let a = 715;
+function increment(a) {
+    return a += 1;
+}
+
+function decrement(a) {
+    return a -= 1;
+}
+
+parseInt('2', 10)
